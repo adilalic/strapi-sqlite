51fe385df112e1accf01dc89ef8d6db646989ae5
Adil-Ali <adilalichacho@gmail.com>
2025-02-17 02:03:31 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,28 @@
 const path = require('path');
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+module.exports = ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  return {
+    connection: isProduction
+      ? {
+          client: 'postgres',
+          connection: {
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT', 5432),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
+            ssl: { rejectUnauthorized: false },
+          },
+          debug: false,
+        }
+      : {
+          client: 'sqlite',
+          connection: {
+            filename: path.join(__dirname, '..', '.tmp/data.db'),
+          },
+          useNullAsDefault: true,
+        },
+  };
+};
